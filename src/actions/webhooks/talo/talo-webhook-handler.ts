6cf8689f36997b495fdfe01c01d6a6e0e6632fbf
Enzo-21 <enzo21barrera@gmail.com>
2025-01-03 18:10:33 -0300

    Hanlde paypal webhooks

--- src/actions/webhooks/talo/talo-webhook-handler.ts
+++ src/actions/webhooks/talo/talo-webhook-handler.ts
@@ -1,3 +1,52 @@
-export const handleTaloWebhook = async (vexor: any, body: any) => {
-    return body;
+import { generateTaloAccessToken } from "@/actions/authorizations/talo/generate-talo-access-token";
+import { retrieveTaloPayment } from "@/actions/retrievals/talo/retrieve-talo-payment";
+import { SUPPORTED_PLATFORMS } from "@/lib/constants";
+import { Vexor } from "@/methods";
+import { SupportedVexorPlatform } from "@/types/platforms";
+import { VexorWebhookResponse } from "@/types/responses";
+
+export const handleTaloWebhook = async (vexor: Vexor, req: any) => {
+
+    const body = await req.json();
+
+    try {
+
+         // Get the platform credentials
+         const platformCredentials = vexor.platforms?.talo
+
+         if (!platformCredentials) {
+             throw new Error('MercadoPago credentials not found');
+         }
+ 
+         const taloUserId = platformCredentials.user_id
+         const taloClientId = platformCredentials.client_id
+         const taloClientSecret = platformCredentials.client_secret
+ 
+         if (!taloUserId || !taloClientId || !taloClientSecret) {
+             throw new Error('Talo credentials not found');
+         }
+
+        // Get talo payment status
+        const taloPaymentData = await retrieveTaloPayment(vexor, body.data.id);
+
+        const response: VexorWebhookResponse = {
+            message: taloPaymentData.message,
+            status: taloPaymentData.status,
+            platform: SUPPORTED_PLATFORMS.TALO.name as SupportedVexorPlatform,
+            identifier: taloPaymentData.id,
+            transmissionId: body.data.id,
+            timeStamp: new Date().toISOString(),
+            orderId: body.data.id,
+            eventType: body.message,
+            resource: taloPaymentData,
+        }
+
+        return response;
+
+    } catch (error) {
+        console.log('Talo webhook body:', body);
+        throw new Error('Talo webhook body is not a valid JSON');
+    }
+
+    console.log('Talo webhook body:', body);
 }
